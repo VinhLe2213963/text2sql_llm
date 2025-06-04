d86121b10b27cea9e22922d6a9375ea8d61c00f1
VinhLe2213963 <167350988+VinhLe2213963@users.noreply.github.com>
2025-06-04 11:28:47 +0000

    Fix Retriever

--- retriever_model.py
+++ retriever_model.py
@@ -9,15 +9,15 @@ class Retriever:
         self.all_questions = all_questions
         self.question_embeddings = self.get_embedding(all_questions)
 
-    def get_embedding(texts):
+    def get_embedding(self, texts):
         inputs = self.tokenizer(texts, return_tensors="pt", padding=True, truncation=True).to(self.device)
         with torch.no_grad():
             outputs = self.model(**inputs)
             cls_embeddings = outputs.last_hidden_state[:, 0]
         return F.normalize(cls_embeddings, p=2, dim=1)
 
-    def get_examples(question, k=5):
-        question_embedding = get_embedding([question])
+    def get_examples(self, question, k=5):
+        question_embedding = self.get_embedding([question])
         similarities = torch.matmul(question_embedding, self.question_embeddings.T).squeeze(0)
         topk_indices = similarities.topk(k).indices.tolist()
         return [self.all_questions[i] for i in topk_indices]
