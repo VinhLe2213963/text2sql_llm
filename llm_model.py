7df5ecb318270e2943b225bf8bb9b532adf59c8f
VinhLe2213963 <167350988+VinhLe2213963@users.noreply.github.com>
2025-06-07 15:15:45 +0000

    Add GPT to llm_model.py

--- llm_model.py
+++ llm_model.py
@@ -2,6 +2,7 @@ import base64
 import os
 from google import genai
 from google.genai import types
+from openai import OpenAI
 from utils import extract_sql_from_llm
 
 class Gemini:
@@ -36,3 +37,31 @@ class Gemini:
             response += chunk.text
 
         return extract_sql_from_llm(response)
+    
+class GPT:
+    def __init__(self, model, endpoint, api):
+        self.model = model
+        self.client = OpenAI(
+            base_url=endpoint,
+            api_key=api,
+        )
+
+    def generate(self, prompt, temperature):
+        response = self.client.chat.completions.create(
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are a helpful assistant.",
+                },
+                {
+                    "role": "user",
+                    "content": prompt,
+                }
+            ],
+            temperature=temperature,
+            top_p=1.0,
+            max_tokens=1000,
+            model=self.model
+        )
+
+        return extract_sql_from_llm(response.choices[0].message.content)
